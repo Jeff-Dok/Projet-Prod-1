6f2fbd5784ac5bfa01e2a505dca4373b5efc1989
Marie Eve Frechette <marevedesign@gmail.com>
2022-03-20 13:54:02 -0400

    Prog complet, il reste a enlever les commentaires apres le review

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,14 @@
-const express = require('express');
+////////////////// !!!!!!! ENLEVER TOUS LES COMMENTAIRES AVANT DE PUBLIER LE FINAL !!!!!!! ////////////////// 
+
+////////////////// CODE PRÉAJUSTEMENTS DE VENDREDI SOIR ////////////////// 
+/*const express = require('express');
 const app = express();
 const axios = require('axios');
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended : true}));
+
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 exports.getRedirect = (req, rep) => {
@@ -19,4 +25,39 @@ exports.sendSignin = (request, response) => {
 };
 exports.sendProfile = (request, response) => {
     response.render("profile");
+};*/
+
+////////////////// CODE AJUSTÉ DE VENDREDI SOIR ////////////////// 
+const express = require('express');
+const app = express();
+//const axios = require('axios');
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+exports.getRedirect = (req, rep) => {
+    rep.redirect("/");
+}
+exports.sendIndex = (request, response) => {
+    response.render("index");
+};
+exports.sendSignup = (request, response) => {
+    response.render("signup");
+};
+exports.sendSignin = (request, response) => {
+    response.render("signin");
+};
+exports.sendProfile = (request, response) => {
+    const data = request.session.profileData;
+    if (data == undefined) {
+        response.redirect("signin")
+    } else {
+        console.log("Rendering profile with token: " + data.token);
+        response.render("profile", {
+            'data': data
+        });
+    }
 };
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const express = require('express');
+////////////////// !!!!!!! ENLEVER TOUS LES COMMENTAIRES AVANT DE PUBLIER LE FINAL !!!!!!! //////////////////
+
+////////////////// CODE PRÉAJUSTEMENTS DE VENDREDI SOIR //////////////////
+/*const express = require('express');
 const app = express();
 const path = require("path");
 const homeController = require("./controllers/homeController");
 const axios = require('axios');
+
 
 app.use(express.urlencoded({
     extended: true
@@ -24,13 +28,14 @@ app.post('/signin', (req, rep) => {
     const password = req.body.password;
     var data = {
         "email": email,
-        "password": password
+        "password": password,
     };
     var config = {
         method: 'post',
         url: 'https://ski-api.herokuapp.com/login',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
         data: data
     };
@@ -53,7 +58,7 @@ app.post('/signin', (req, rep) => {
 
             axios(config)
                 .then(function (r) {
-                    rep.render('profile', {
+                rep.render('profile', {
                         'data': r.data,
                     });
                 })
@@ -97,6 +102,161 @@ app.post('/signup', (req, rep) => {
             rep.render('profile', {
                 'data': response.data
             });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+});
+
+app.listen(3000, () => {
+    console.log("Le serveur est sur le port 3000");
+});*/
+
+////////////////// CODE AJUSTÉ DE VENDREDI SOIR //////////////////
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const path = require("path");
+const homeController = require("./controllers/homeController");
+const axios = require("axios");
+
+app.use(
+    session({
+        secret: "SecretToken",
+    })
+);
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", homeController.sendIndex);
+app.get("/index", homeController.getRedirect);
+app.get("/signup", homeController.sendSignup);
+app.get("/signin", homeController.sendSignin);
+app.get("/profile", homeController.sendProfile);
+
+//PARTIE SIGNIN
+app.post("/signin", (req, rep) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const data = {
+        email: email,
+        password: password,
+    };
+    const config = {
+        method: "post",
+        url: "https://ski-api.herokuapp.com/login",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        data: data,
+    };
+    axios(config)
+        .then(function (response) {
+            let token = response.data.token;
+            const data = {
+                token: token,
+            };
+            req.session.skiApiToken = token;
+            const config = {
+                method: "get",
+                url: "https://ski-api.herokuapp.com/tokenInfo",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                    Accept: "application/json",
+                },
+                data: data,
+            };
+
+            axios(config)
+                .then(function (response) {
+                    req.session.profileData = response.data;
+                    rep.redirect("profile");
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        });
+});
+
+//PARTIE SIGNUP
+app.post("/signup", (req, rep) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    //
+
+    const initialData = {
+        name: name,
+        email: email,
+        password: password,
+    };
+
+    const config = {
+        method: "post",
+        url: "https://ski-api.herokuapp.com/signup",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        data: initialData,
+    };
+
+    axios(config)
+        .then(function () {
+            //response to signup - signin now to get the token
+            const config = {
+                method: "post",
+                url: "https://ski-api.herokuapp.com/login",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                data: initialData,
+            };
+            axios(config)
+                .then(function (response) {
+                    let token = response.data.token;
+                    const data = {
+                        token: token,
+                    };
+                    req.session.skiApiToken = token;
+                    const config = {
+                        method: "get",
+                        url: "https://ski-api.herokuapp.com/tokenInfo",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: token,
+                            Accept: "application/json",
+                        },
+                        data: data,
+                    };
+
+                    axios(config)
+                        .then(function (response) {
+                            req.session.profileData = response.data;
+                            rep.redirect("profile");
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                })
+
+                .catch(function (error) {
+                    console.log(error);
+                });
         })
         .catch(function (error) {
             console.log(error);
